39899eab6002f629e81c4edbaeb0c3beb5eab665
Rose Ramirez <44708382+rosethepose@users.noreply.github.com>
2021-12-09 13:37:08 -0800

    Add files via upload

--- MentalHealthApp/Shared/ContentView.swift
+++ MentalHealthApp/Shared/ContentView.swift
@@ -1,16 +1,51 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by cpsc on 12/7/21.
-//
-
 import SwiftUI
+let checkListData = [
+    CheckListItem(id:0,title: "Goal 1"),
+    CheckListItem(id:1,title:"Goal 2"),
+    CheckListItem(id:2,title:"Goal 3")
+]
 
+struct CheckListItem : Identifiable{
+    var id: Int
+    var isChecked: Bool = false
+    var title: String
+    
+ }
+
+struct CheckView: View {
+    @State var isChecked:Bool = false
+    var title:String
+    func toggle(){isChecked = !isChecked}
+    var body: some View {
+        HStack{
+            Button(action: toggle) {
+                Image(systemName: isChecked ? "checkmark.circle" : "circle")
+            }
+            Text(title)
+        }
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack{
+            Text("Mental Health Dashboard").font(.title).fontWeight(.bold)
+            Spacer()
+            VStack{ //list of goals
+                Text("My goals")
+                List(checkListData){ item in CheckView(isChecked: item.isChecked, title: item.title)
+                        }
+                //on click send to goal page
+            }
+            Spacer()
+            VStack {
+                Text("My week")
+                HStack{
+                    //week view
+                }
+            }
+            Spacer()
+        }
     }
 }
 
